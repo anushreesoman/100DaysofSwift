fa269623eba03d1c1e1143335dabccd55c616171
Anushree Soman <anuso.deepak@gmail.com>
2023-04-18 12:59:55 -0400

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Anushree Soman on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
